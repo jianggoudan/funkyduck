85d66492015886a62a76eb0badddbd7a0464b53e
jianggoudan <new5spring@126.com>
2020-05-09 23:37:58 +1200

    unity project

---
+++ DestroyTheStars/DestroyTheStars/Assets/Scripts/GameCtrl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class GameCtrl : MonoBehaviour
+{
+    public static GameCtrl inst;
+
+    public GameObject endUI;
+    public Text scoreLabel;
+    public string nextScene;
+
+    private PropCtrl[] propCtrls;
+    private int score = 0;
+    private void Awake()
+    {
+        inst = this;
+    }
+
+    void Start()
+    {
+        propCtrls = GameObject.FindObjectsOfType<PropCtrl>();
+    }
+
+    public  void  JudgeSuccess()
+    {
+        score++;
+        scoreLabel.text = score.ToString();
+        if (score < propCtrls.Length) return;
+
+        LoadScene(nextScene);
+    }
+    public void OnFailed()
+    {
+        endUI.SetActive(true);
+    }
+
+    public void LoadScene(string _name)
+    {
+        SceneManager.LoadScene(_name);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ DestroyTheStars/DestroyTheStars/Assets/Scripts/PlayerCtrl.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    public Animator animator;
+    public Rigidbody2D playerRB;
+    public Transform bottom;
+
+    public float moveSpeed = 1;
+    public float jumpForce = 5;
+    public LayerMask checkLayer;
+    public float checkRadius = 0.1f;
+
+    private bool isGround, isJump;
+    private bool jumpPressed;
+    private int jumpCount;
+
+    void Start()
+    {
+        if (animator == null)
+            animator = gameObject.GetComponent<Animator>();
+        if (playerRB == null)
+            playerRB = gameObject.GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCount > 0)
+        {
+            jumpPressed = true;
+        }
+    }
+    void FixedUpdate()
+    {
+        isGround = Physics2D.OverlapCircle(bottom.position, checkRadius, checkLayer);
+        GroundMovement();
+        Jump();
+        SwitchAnim();
+    }
+    /// <summary>
+    /// 移动
+    /// </summary>
+    void GroundMovement()
+    {
+        float h = Input.GetAxisRaw("Horizontal");//只有0 -1 1 三个数字
+        playerRB.velocity = new Vector2(h * moveSpeed, playerRB.velocity.y);
+
+        if (h != 0)
+            transform.localScale = new Vector3(h * 2, 2, 2);//控制翻转
+    }
+    /// <summary>
+    /// 跳跃
+    /// </summary>
+    void Jump()
+    {
+        if (isGround)
+        {
+            jumpCount = 2;
+            isJump = false;
+        }
+
+        if (jumpPressed && isGround)
+        {
+            isJump = true;
+            playerRB.velocity = new Vector2(playerRB.velocity.x, jumpForce);
+            jumpCount--;
+            jumpPressed = false;//每0.02秒检测按键是否松开
+        }
+        else if (jumpPressed && jumpCount > 0 && !isGround)
+        {
+            playerRB.velocity = new Vector2(playerRB.velocity.x, jumpForce);
+            jumpCount--;
+            jumpPressed = false;
+        }
+
+    }
+    /// <summary>
+    /// 动画
+    /// </summary>
+    private void SwitchAnim()
+    {
+        animator.SetFloat("Move", playerRB.velocity.x);
+        animator.SetBool("Jump", !isGround);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name.Contains("Star"))
+        {
+            Destroy(collision.gameObject);
+            GameCtrl.inst.JudgeSuccess();
+        }
+        else if (collision.name.Contains("Trap"))
+        {
+            playerRB.velocity = Vector2.zero;
+            playerRB.isKinematic = true;
+            animator.SetBool("Die",true);
+            GameCtrl.inst.OnFailed();
+        }
+    }
+}
