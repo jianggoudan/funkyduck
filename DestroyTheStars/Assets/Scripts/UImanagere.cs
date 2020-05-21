15f38d229bc2dbc30972a7fece93a9cda56ddac8
jianggoudan <new5spring@126.com>
2020-05-21 17:43:04 +1200

    life line

---
+++ DestroyTheStars/Assets/Scripts/UImanagere.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UImanagere : MonoBehaviour
+{
+    public static UImanagere instance { get; private set; }
+    public Image healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+   public void UpdateHealthBar()
+    {
+        healthBar.fillAmount = 0.0f;
+    }
+}
