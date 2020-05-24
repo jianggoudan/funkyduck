9d5acd4fa624ff8160798b8d03b111a74402b2d9
Dingguang Lyu <55654890+gg112141@users.noreply.github.com>
2020-05-24 13:09:28 +1200

    music and mute functions-lyu

---
+++ DestroyTheStars/Assets/muteManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class muteManager : MonoBehaviour
+{
+	private bool isMuted;
+
+    void Start()
+    {
+		isMuted = false;
+    }
+
+    public void MutePressed()
+	{
+		isMuted = !isMuted;
+	    AudioListener.pause = isMuted;
+		
+
+	}
+
+}
