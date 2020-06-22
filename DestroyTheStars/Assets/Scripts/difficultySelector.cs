6e985af9529861b137db59c1f0fb00536aa936b8
jianggoudan <new5spring@126.com>
2020-06-23 01:28:43 +1200

    d'f'c'x'z

---
+++ DestroyTheStars/Assets/Scripts/difficultySelector.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class difficultySelector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void hard()
+    {
+        SceneManager.LoadScene("Level5");
+    }
+    public void medium()
+    {
+        SceneManager.LoadScene("Level4");
+    }
+
+    public void easy()
+    {
+        SceneManager.LoadScene("Level0");
+    }
+    public void back()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
